f7ccee96a7963d879b3fb58ad910df05b860f19e
tmdnth <tmdnth@gmail.com>
2013-07-23 20:03:37 +0200

    Rewrote the NextPrime code, because holy fuck.

--- Numbers/NextPrime.lua
+++ Numbers/NextPrime.lua
@@ -6,32 +6,21 @@ until the user chooses to stop asking for the next one.
 primes = {}
 current = 1
 
-while(true) do
-	print("Output another prime?")
-	if io.read() == "y" then
-		noNewPrime = true
+repeat
 
-		while noNewPrime do
-			current = current + 1
-			notPrime = false
+  repeat
+    current = current + 1
+    mod = -1
+    for i, v in ipairs(primes) do
+      mod = current % v
+      if mod == 0 then break end
+    end
 
-			-- Yes, we're inefficient.
-			if #primes ~= 0 then
-				for index, value in ipairs(primes) do
-					if current % value == 0 then
-						notPrime = true
-						break
-					end
-				end
-			end
+    if mod ~= 0 then
+      table.insert(primes, current)
+      print("Next prime is " .. current)
+    end
+  until mod ~= 0
 
-			if not notPrime then
-				print("\nHere it is!\n" .. current .. "\n")
-				table.insert(primes, current)
-				noNewPrime = false
-			end
-		end
-	else
-		break
-	end
-end
+  print("Output another prime?")
+until io.read() ~= "y"
